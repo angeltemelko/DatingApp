7487913e77acc28159b3762a909dedc8d8dd70ef
angetemelko <angel_aleksandar@hotmail.com>
2022-03-07 18:47:30 +0100

    Created backend part

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dbContext;
+
+        public UsersController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _dbContext.Users.ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser([FromRoute] int id)
+        {
+            return await _dbContext.Users.FindAsync(id);
+        }
+    }
+}
