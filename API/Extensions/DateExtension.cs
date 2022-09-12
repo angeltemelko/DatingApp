8b1d95b4ff3d535b7f6850b71088a2d71c2a8fe5
angetemelko <angel_aleksandar@hotmail.com>
2022-09-12 22:34:02 +0200

    -Added new properties for our user, created collection of photos, seeds for DB and new migration

---
+++ API/Extensions/DateExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace API.Extensions
+{
+    public static class DateExtension
+    {
+        public static int GetAge(this DateTime dob)
+        {
+            int age = DateTime.Today.Year - dob.Year;
+
+            if (dob.Date > DateTime.Today.AddYears(-age)) 
+                age--;
+            
+            return age;
+        }
+    }
+}
